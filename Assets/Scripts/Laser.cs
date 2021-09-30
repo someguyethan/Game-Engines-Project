b4a780e5720a45e3fd4c82d39309a5353e4044bf
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-09-30 17:01:57 -0400

    Added Win/Lose Screen, Main Menu

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene("Defeat");
         Destroy(collision.gameObject);
     }
 }
