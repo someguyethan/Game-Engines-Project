74906235e954ae31d352357e8e04a751eb3bb6aa
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-09-30 17:24:30 -0400

    Added Functional Menu Buttons

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NextScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GameScene");
     }
 }
