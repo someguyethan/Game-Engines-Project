9ca557f6aca8de7e0a1d2000229cad190fea168f
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-09-30 16:41:38 -0400

    Added Empty Scenes

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene("Victory");
+    }
+}
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spikes : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
