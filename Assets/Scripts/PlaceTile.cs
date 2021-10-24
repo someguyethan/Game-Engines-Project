4a57b7f62172fbaf998c5550bdb30e57aff10c6e
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-10-24 16:11:51 -0400

    Proper Tile Preview

--- Assets/Scripts/PlaceTile.cs
+++ Assets/Scripts/PlaceTile.cs
@@ -10,11 +10,15 @@ public class PlaceTile : MonoBehaviour
     public TileBase groundPreview;
 
     Vector3Int temp = Vector3Int.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    Vector2Int[] directions = new Vector2Int[] { new Vector2Int(1,0),
+                                                 new Vector2Int(1,-1),
+                                                 new Vector2Int(0,-1),
+                                                 new Vector2Int(-1,-1),
+                                                 new Vector2Int(-1,0),
+                                                 new Vector2Int(-1,1),
+                                                 new Vector2Int(0,1),
+                                                 new Vector2Int(1,1)};
 
     // Update is called once per frame
     void Update()
@@ -22,9 +26,23 @@ public class PlaceTile : MonoBehaviour
         Vector2 screenPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
         Vector3 worldPos3 = new Vector3 (worldPos.x, worldPos.y, 0f);
-       
+        
+
         if (map.GetTile(map.WorldToCell(worldPos3)) == null)
+        {
             map.SetTile(map.WorldToCell(worldPos3), groundPreview);
+            //reset all others
+            for (int i = 1; i <= 8; i++)
+            {
+                Vector3Int temp = new Vector3Int(map.WorldToCell(worldPos3).x + directions[i - 1].x,
+                                                 map.WorldToCell(worldPos3).y + directions[i - 1].y,
+                                                 0);
+                if (map.GetTile(temp) == groundPreview)
+                {
+                    map.SetTile(temp, null);
+                }
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
